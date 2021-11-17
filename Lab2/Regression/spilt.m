97d66759a79ae70630ba6099ff39c37bf85b263a
Jialong Pei <ppxjp1@exmail.nottingham.ac.uk>
2021-11-17 09:40:17 +0000

    Create spilt.m

---
+++ Lab2/Regression/spilt.m
@@ -0,0 +1,23 @@
+function [ DLeft, DRight ] = Spilt( dataSet, feature, value )
+  
+    [m,~] = size(dataSet);
+    DataTemp = dataSet(:,feature)';
+    
+    indexAll = 1:m;
+    indexLeft = indexAll(DataTemp > value);
+    indexRight = indexAll(DataTemp <= value);
+    
+    [~,nLeft] = size(indexLeft);
+    [~,nRight] = size(indexRight);
+    
+    if nLeft>0 && nRight>0
+        DLeft = dataSet(indexLeft,:);
+        DRight = dataSet(indexRight,:);
+    elseif nLeft == 0
+         DLeft = [];
+         DRight = dataSet;
+    elseif nRight == 0
+         DRight = [];
+         DLeft = dataSet;
+    end
+end
