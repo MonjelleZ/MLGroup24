8260e905020637f1732dc3977ac3a9ac347e00c7
Yanzheng <psxyl37@nottingham.ac.uk>
2021-11-20 12:37:32 +0000

    accuracy modefy

--- Lab2/Classification/F1_recall_precision.m
+++ Lab2/Classification/F1_recall_precision.m
@@ -1,37 +1,24 @@
-function [F1,recal, preci,accu] = F1_recall_precision(targets, outputs,class)
+function [F1,recal, preci,accu] = F1_recall_precision(targets, outputs)
 tp = 0;
 fp = 0;
 tn = 0;
 fn = 0;
-if ischar(class)
-    for i = 1:size(targets,2)
-        if strcmp(targets(i),class) && strcmp(outputs(i).calss)
-            tp = tp+1;
-        elseif strcmp(targets(i),class) && ~strcmp(outputs(i).calss)
-            fn = fn+1;
-        elseif ~strcmp(targets(i),class) && strcmp(outputs(i).calss)
-            fp = fp+1;
-        else
-            tn = tn+1
-        end
-    end
-end
-if isnumeric(class)
-    s = size(targets,2)
-    for i = 1:s
-        if targets(i) == class && outputs(i) == class
-            tp = tp+1;
-        elseif targets(i) == class && outputs(i) ~= class
-            fn = fn+1;
-        elseif targets(i) ~= class && outputs(i) == class
-            fp = fp+1;
-        else
-            tn = tn+1
-        end
-    end
 
-end
-    accu = (tp+fp)/(tp+fp+tn+fn);
+
+s = size(targets,1)
+  for i = 1:s
+        if targets(i) == 1 && outputs(i) == 1
+            tp = tp+1;
+        elseif targets(i) == 1 && outputs(i) ~= 1
+            fn = fn+1;
+        elseif targets(i) ~= 1 && outputs(i) == 1
+            fp = fp+1;
+        else
+            tn = tn+1
+        end
+  end
+
+    accu = (tp+tn)/(tp+fp+tn+fn);
     recal = tp/(tp+fn)
     preci = tp/(tp+fp); 
     F1 = 2*preci*recal/(preci+recal);
