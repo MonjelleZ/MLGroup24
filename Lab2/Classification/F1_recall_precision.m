070fa9eafb1dc71f7dca14a709593a2e35ce8800
Monjelle <mengjiao@mengjiaodeMacBook-Air.local>
2021-11-20 22:25:46 +0800

    fix bug

--- Lab2/Classification/F1_recall_precision.m
+++ Lab2/Classification/F1_recall_precision.m
@@ -1,11 +1,15 @@
 function [F1,recal, preci,accu] = F1_recall_precision(targets, outputs)
+
+%  targets : real dataset
+%  outputs : predict dataset
+
 tp = 0;
 fp = 0;
 tn = 0;
 fn = 0;
 
 
-s = size(targets,1)
+s = size(targets,1);
   for i = 1:s
         if targets(i) == 1 && outputs(i) == 1
             tp = tp+1;
@@ -14,12 +18,12 @@ s = size(targets,1)
         elseif targets(i) ~= 1 && outputs(i) == 1
             fp = fp+1;
         else
-            tn = tn+1
+            tn = tn+1;
         end
   end
 
     accu = (tp+tn)/(tp+fp+tn+fn);
-    recal = tp/(tp+fn)
+    recal = tp/(tp+fn);
     preci = tp/(tp+fp); 
     F1 = 2*preci*recal/(preci+recal);
 end
